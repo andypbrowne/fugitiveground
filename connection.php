894d9a9be888e8e2b62671b16039af8d061379e0
Andrew Paul Browne <andypbrowne@gmail.com>
2015-03-02 16:00:19 -0500

    Update connection.php
    removed database password information (passwords available in backup).

--- connection.php
+++ connection.php
@@ -3,9 +3,9 @@
  * Date: 05/15/2014
  */
     $mysql_hostname = "localhost";
-    $mysql_user = "andypbro_fgadmin";
-    $mysql_password = "butterflyanchor!!@12";
-    $mysql_database = "andypbro_db_fugitiveground";
+    $mysql_user = "#";
+    $mysql_password = "#";
+    $mysql_database = "#";
     $prefix = "";
     $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
     mysql_select_db($mysql_database, $bd) or die("Could not select database");
